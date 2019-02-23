502764d11c66681f7b6c1e8f9572cf03ca49840a
Sandro  Mello <sandromll@gmail.com>
2019-02-23 14:39:21 -0300

    first commit

---
+++ Makefile
@@ -0,0 +1,14 @@
+SHORT_NAME ?= discounts-api
+
+include versioning.mk
+
+test:
+	docker run --rm -it ${REGISTRY}/${IMAGE_PREFIX}/${SHORT_NAME}:${VERSION} scala-sbt clean test
+
+docker-build:
+	docker build -t ${REGISTRY}/${IMAGE_PREFIX}/${SHORT_NAME}:${VERSION} .
+
+docker-push:
+	docker push ${REGISTRY}/${IMAGE_PREFIX}/${SHORT_NAME}:${VERSION}
+
+.PHONY: test docker-build docker-push
---
+++ versioning.mk
@@ -0,0 +1,16 @@
+GITCOMMIT_SHORT = $(shell git rev-parse --short HEAD)
+GIT_TAG ?= $(or ${TRAVIS_TAG},${TRAVIS_TAG},${GITCOMMIT_SHORT})
+VERSION ?= ${GIT_TAG}
+GITCOMMIT ?= $(shell git rev-parse HEAD)
+
+REGISTRY ?= quay.io
+IMAGE_PREFIX ?= sandromello
+
+IMAGE := ${REGISTRY}/${IMAGE_PREFIX}/${SHORT_NAME}:${VERSION}
+
+all: docker-build docker-push
+
+info:
+	@echo "Build tag:       ${VERSION}"
+	@echo "Registry:        ${REGISTRY}"
+	@echo "Immutable tag:   ${IMAGE}"
